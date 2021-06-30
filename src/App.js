bddc1d95b0d339d8a35d0e2f686e870a189cb991
Meghant00 <dangolmeghant0@gmail.com>
2021-06-30 13:12:23 +0545

    Chore: added tailwind and react
    Chore: added tailwind and react

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="px-10 py-16 text-2xl font-semibold">React</div>
+    </div>
+  );
+}
+
+export default App;
