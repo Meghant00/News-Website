fc3e443c23f569a341b7c23bf77ad982410e5a8a
Meghant00 <dangolmeghant0@gmail.com>
2021-07-02 12:56:17 +0545

    Style
    Added new animation to search bar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import "./App.css";
 import Header from "./components/header";
 function App() {
-  return <Header />;
+  return (
+    <>
+      <Header />
+    </>
+  );
 }
 
 export default App;
--- src/components/search-bar.js
+++ src/components/search-bar.js
@@ -1,26 +1,27 @@
 import React from "react";
-import { useSpring, animated } from "react-spring";
+import { useTransition, animated } from "react-spring";
 const SearchBar = ({ searchClicked }) => {
-  const animation = useSpring({
-    config: {
-      duration: 1000,
-    },
-    transform: searchClicked ? `translateY(0%)` : "translateY(-100%)",
+  const transition = useTransition(searchClicked, {
+    from: { y: -100 },
+    enter: { y: 0 },
+    leave: { y: -100 },
   });
   return (
     <div className="w-full">
-      {searchClicked ? (
-        <animated.div style={animation}>
-          <form className="w-1/2 py-2 ml-auto  bg-white  border-l border-black ">
-            <input
-              type="text"
-              className="px-2 text-sm w-full focus:outline-none text-gray-600"
-              placeholder="Search..."
-            />
-          </form>
-        </animated.div>
-      ) : (
-        ""
+      {transition((style, item) =>
+        item ? (
+          <animated.div style={style}>
+            <form className="w-1/2 py-2 ml-auto  bg-white  border-l border-black ">
+              <input
+                type="text"
+                className="px-2 text-sm w-full focus:outline-none text-gray-600"
+                placeholder="Search..."
+              />
+            </form>
+          </animated.div>
+        ) : (
+          ""
+        )
       )}
     </div>
   );
