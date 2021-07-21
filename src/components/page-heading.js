67c16e1d6b7b5845f75b6abda609db861b31345b
Meghant00 <dangolmeghant0@gmail.com>
2021-07-21 09:26:08 +0545

    Style: Add Page Heading Component

---
+++ src/components/page-heading.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const PageHeading = ({ heading }) => {
+  return (
+    <div className="flex flex-row items-center justify-center">
+      <img src="/images/patern-2.png" alt="patern-2" />
+      <h2 className="uppercase lg:text-5xl py-4">{heading}</h2>
+      <img src="/images/patern-2.png" alt="patern-2" />
+    </div>
+  );
+};
+
+export default PageHeading;
