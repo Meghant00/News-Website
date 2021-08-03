881c619af4c131d6a72cf667bef2d858015a281d
Meghant00 <dangolmeghant0@gmail.com>
2021-08-03 13:23:17 +0545

    Style: Change hover of category Item from black to title

--- src/components/category-item.js
+++ src/components/category-item.js
@@ -1,7 +1,16 @@
 import React from "react";
 
 const CategoryItem = ({ category }) => {
-  return <div className="py-2 px-2 text-lg">{category}</div>;
+  return (
+    <div className="py-2 px-2 text-lg">
+      <a
+        href={category}
+        className="transition duration-100 ease-linear hover:text-title "
+      >
+        {category}
+      </a>
+    </div>
+  );
 };
 
 export default CategoryItem;
