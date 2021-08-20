63c6d9001317177c8352e7ddb59ab8413a0d4a2a
Meghant00 <dangolmeghant0@gmail.com>
2021-08-20 13:05:53 +0545

    Feat: Add Routing to home

--- src/App.js
+++ src/App.js
@@ -1,9 +1,83 @@
 import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { useState, useRef } from "react";
+import Sidebar from "./components/sidebar";
+import Header from "./components/header";
+import Title from "./components/title";
+import Tagline from "./components/tagline";
+import NavMenu from "./components/nav-menu";
+import TopSlider from "./components/top-slider";
 import Home from "./components/home";
+import PopularArticles from "./components/popular-articles";
+import Search from "./components/search";
+import Trending from "./components/trending";
+import HotTags from "./components/hot-tags";
+import DontMiss from "./components/dontMiss";
+import Latest from "./components/latest";
+import Categories from "./components/categories";
+import Footer from "./components/footer";
 function App() {
+  const [sidebarClicked, setSidebarClicked] = useState(false);
+  const sidebarClickHandler = () => {
+    setSidebarClicked(!sidebarClicked);
+  };
+
+  const close = (e) => {
+    if (sidebarRef.current === e.target) {
+      setSidebarClicked(false);
+    }
+  };
+
+  const sidebarRef = useRef();
   return (
     <>
-      <Home />
+      <Router>
+      <div className="relative">
+      <Sidebar
+        sidebarClicked={sidebarClicked}
+        setSidebarClicked={setSidebarClicked}
+      />
+        {sidebarClicked ? (
+          <div
+            ref={sidebarRef}
+            onClick={close}
+            className="h-full w-full bg-white bg-opacity-70 absolute top-0 z-20"
+          ></div>
+        ) : (
+          ""
+        )}
+
+<div>
+        <Header sidebarClickHandler={sidebarClickHandler} />
+        <div className="w-4/5 mx-auto">
+          <Title name="Hello World" />
+          <Tagline />
+          <NavMenu />
+          <TopSlider />
+          <div className="grid grid-cols-1 lg:grid-cols-3 gap-4 py-4 ">
+            <div className="col-span-0 lg:col-span-2  lg:border-double lg:border-r-4 lg:border-gray-300">
+            <Switch>
+          <Route path={"/"} exact component={Home} />
+        </Switch>
+            </div>
+            <div className="">
+              <PopularArticles />
+              <Search />
+              <Trending />
+              <HotTags />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-4 w-4/5 mx-auto py-4 border-b-2 border-black">
+        <DontMiss />
+        <Latest />
+        <Categories />
+      </div>
+      <Footer />
+    </div>
+        
+      </Router>
     </>
   );
 }
--- src/components/home.js
+++ src/components/home.js
@@ -1,77 +1,11 @@
 import React from "react";
-import Header from "./header";
-import Sidebar from "./sidebar";
-import { useState, useRef } from "react";
-import Title from "./title";
-import Tagline from "./tagline";
-import NavMenu from "./nav-menu";
-import TopSlider from "./top-slider";
 import MainSlider from "./main-slider";
 import RecentPosts from "./recent-posts";
-import PopularArticles from "./popular-articles";
-import Search from "./search";
-import Trending from "./trending";
-import HotTags from "./hot-tags";
-import DontMiss from "./dontMiss";
-import Latest from "./latest";
-import Categories from "./categories";
-import Footer from "./footer";
 const Home = () => {
-  const [sidebarClicked, setSidebarClicked] = useState(false);
-  const sidebarClickHandler = () => {
-    setSidebarClicked(!sidebarClicked);
-  };
-
-  const close = (e) => {
-    if (sidebarRef.current === e.target) {
-      setSidebarClicked(false);
-    }
-  };
-
-  const sidebarRef = useRef();
   return (
-    <div className="relative">
-      <Sidebar
-        sidebarClicked={sidebarClicked}
-        setSidebarClicked={setSidebarClicked}
-      />
-        {sidebarClicked ? (
-          <div
-            ref={sidebarRef}
-            onClick={close}
-            className="h-full w-full bg-white bg-opacity-70 absolute top-0 z-20"
-          ></div>
-        ) : (
-          ""
-        )}
-
-<div>
-        <Header sidebarClickHandler={sidebarClickHandler} />
-        <div className="w-4/5 mx-auto">
-          <Title name="Hello World" />
-          <Tagline />
-          <NavMenu />
-          <TopSlider />
-          <div className="grid grid-cols-1 lg:grid-cols-3 gap-4 py-4 ">
-            <div className="col-span-0 lg:col-span-2  lg:border-double lg:border-r-4 lg:border-gray-300">
-              <MainSlider />
-              <RecentPosts />
-            </div>
-            <div className="">
-              <PopularArticles />
-              <Search />
-              <Trending />
-              <HotTags />
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="grid grid-cols-1 lg:grid-cols-3 gap-4 w-4/5 mx-auto py-4 border-b-2 border-black">
-        <DontMiss />
-        <Latest />
-        <Categories />
-      </div>
-      <Footer />
+    <div>
+      <MainSlider />
+      <RecentPosts />
     </div>
   );
 };
