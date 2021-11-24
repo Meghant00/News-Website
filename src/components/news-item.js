d5e531782576b87218f7810c6b5b0acb4415ab5a
Meghant00 <dangolmeghant0@gmail.com>
2021-11-24 15:52:47 +0545

    Style: Changed Style of News-Items

--- src/components/news-item.js
+++ src/components/news-item.js
@@ -7,28 +7,28 @@ const NewsItem = ({ image, title, author, authorImage, tags, date }) => {
   const month = displayDate.toLocaleString("default", { month: "short" });
   const year = displayDate.getFullYear();
   return (
-    <div className="flex flex-row justify-between">
-      <div className="bg-black w-2/5">
-        <img src={image} alt={title} />
+    <div className="flex flex-row justify-between py-4">
+      <div className=" w-2/6">
+        <img src={image} alt={title} className="w-full" />
       </div>
-      <div className="px-8 flex flex-col items-center justify-between">
-        <h3 className="text-2xl font-semibold tracking-wider hover:text-title">
+      <div className="px-8 flex flex-col items-start justify-start w-4/6">
+        <h3 className="text-xl font-semibold tracking-wider hover:text-title">
           <a href="home">{title}</a>
         </h3>
-        <div className="flex flex-row items-center justify-between text-gray-400 space-x-4">
+        <div className="flex flex-row items-center justify-between text-gray-400 space-x-4 py-2">
           <div className="flex items-center justify-between">
             <img
               src={authorImage}
               alt={author}
               className="rounded-full w-8 h-8"
             />
-            <span className="text-sm">{author}</span>
+            <span className="text-sm px-2">{author}</span>
           </div>
           <div className="flex flex-row items-center justify-start uppercase">
             <Tags tags={tags} />
           </div>
           <div>
-            {day}, {month}, {year}
+            {day} {month}, {year}
           </div>
         </div>
       </div>
