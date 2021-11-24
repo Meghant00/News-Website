8160b46ae11e33179b63747a7110ac3440f2ae92
Meghant00 <dangolmeghant0@gmail.com>
2021-11-24 15:37:43 +0545

    Create News-List Component

---
+++ src/components/news-list.js
@@ -0,0 +1,23 @@
+import React from "react";
+import news from "../data/news";
+import NewsItem from "./news-item";
+
+const NewsList = () => {
+  return (
+    <div>
+      {news.map((items) => (
+        <NewsItem
+          key={items.title}
+          title={items.title}
+          author={items.author}
+          image={items.image}
+          authorImage={items.authorImage}
+          date={items.date}
+          tags={items.tags}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default NewsList;
