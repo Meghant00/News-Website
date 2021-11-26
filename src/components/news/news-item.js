8bc2a51bbb4d027b4b1cc505895d9151905ef57d
Meghant00 <dangolmeghant0@gmail.com>
2021-11-26 16:02:50 +0545

    Style: Made news-item mobile responsive

--- src/components/news/news-item.js
+++ src/components/news/news-item.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { FaFolder } from "react-icons/fa";
 import Tags from "../tags";
 
 const NewsItem = ({ image, title, author, authorImage, tags, date }) => {
@@ -7,16 +8,16 @@ const NewsItem = ({ image, title, author, authorImage, tags, date }) => {
   const month = displayDate.toLocaleString("default", { month: "short" });
   const year = displayDate.getFullYear();
   return (
-    <div className="flex flex-row justify-between py-10">
-      <div className=" w-2/6">
+    <div className="flex flex-col justify-between py-10 md:flex-row">
+      <div className="md:w-2/6">
         <img src={image} alt={title} className="w-full" />
       </div>
-      <div className="px-8 flex flex-col items-start justify-start w-4/6">
-        <h3 className="text-xl font-semibold tracking-wider hover:text-title">
+      <div className="flex flex-col items-start justify-start md:w-4/6 md:px-8">
+        <h3 className="text-lg font-semibold tracking-wider hover:text-title md:text-xl">
           <a href="home">{title}</a>
         </h3>
-        <div className="flex flex-row items-center justify-between text-gray-400 space-x-4 py-2">
-          <div className="flex items-center justify-between">
+        <div className="flex flex-col text-gray-600 py-2 space-y-2 md:space-y-0 md:space-x-4 md:flex-row md:items-center md:justify-between md:text-gray-400">
+          <div className="flex items-center justify-start">
             <img
               src={authorImage}
               alt={author}
@@ -25,6 +26,7 @@ const NewsItem = ({ image, title, author, authorImage, tags, date }) => {
             <span className="text-sm px-2">{author}</span>
           </div>
           <div className="flex flex-row items-center justify-start uppercase">
+            <FaFolder className="flex md:invisible" />
             <Tags tags={tags} />
           </div>
           <div>
