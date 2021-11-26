1b7ae4e0f68747c831d8d8ec20dbd4c2287839cc
Meghant00 <dangolmeghant0@gmail.com>
2021-11-26 15:53:07 +0545

    Style: Changed Responsive design of top-news-item
    Changed font sized from xs to sm in small devices.
    Changed text color from gray-400 to gray-600
    Changed gap-y-0 to gap-y-4

--- src/components/news/top-news-item.js
+++ src/components/news/top-news-item.js
@@ -16,20 +16,20 @@ const TopNews = ({
   const month = displayDate.toLocaleString("default", { month: "short" });
   const year = displayDate.getFullYear();
   return (
-    <div className="flex flex-row items-center justify-between gap-4 px-2 py-4">
-      <div className="w-2/5">
+    <div className="flex flex-col items-center justify-between gap-4 md:flex-row px-2 py-4">
+      <div className="md:w-2/5">
         <a href={heading}>
           <img src={image} alt={heading} />
         </a>
       </div>
-      <div className="flex flex-col items-start justify-between w-3/5 text-gray-400">
+      <div className="flex flex-col items-start justify-between md:w-3/5 text-gray-400">
         <a
           href={heading}
           className="text-black hover:text-title transition duration-150 ease-in"
         >
           <h4 className="text-xl font-semibold">{heading}</h4>
         </a>
-        <div className="flex flex-col md:flex-row items-start md:items-center lg:gap-4 justify-between text-xs font-semibold py-4 ">
+        <div className="flex flex-col text-md gap-y-4 text-gray-600 items-start justify-between font-semibold py-4 md:text-gray-400 md:gap-y-0 md:text-xs md:flex-row  md:items-center lg:gap-4">
           <a
             href={author}
             className="hover:text-blue-700 transition duration-100 ease-linear 
