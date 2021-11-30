bd031af47d08e98b92512c141e11c20f37f24260
Meghant00 <dangolmeghant0@gmail.com>
2021-11-30 15:45:43 +0545

    Style: Made news item responsive on medium screen

--- src/components/news/news-item.js
+++ src/components/news/news-item.js
@@ -8,12 +8,12 @@ const NewsItem = ({ image, title, author, authorImage, tags, date }) => {
   const month = displayDate.toLocaleString("default", { month: "short" });
   const year = displayDate.getFullYear();
   return (
-    <div className="flex flex-col justify-between py-10 md:flex-row">
-      <div className="md:w-2/6">
+    <div className="flex flex-col justify-between items-start py-10 lg:flex-row">
+      <div className="lg:w-2/6">
         <img src={image} alt={title} className="w-full" />
       </div>
-      <div className="flex flex-col items-start justify-start md:w-4/6 md:px-8">
-        <h3 className="text-lg font-semibold tracking-wider hover:text-title md:text-xl">
+      <div className="flex flex-col items-start justify-start lg:px-8 lg:w-4/6">
+        <h3 className="text-lg font-semibold tracking-wider hover:text-title md:text-xl md:py-4 lg:py-0">
           <a href="home">{title}</a>
         </h3>
         <div className="flex flex-col text-gray-600 py-2 space-y-2 md:space-y-0 md:space-x-4 md:flex-row md:items-center md:justify-between md:text-gray-400">
@@ -26,7 +26,7 @@ const NewsItem = ({ image, title, author, authorImage, tags, date }) => {
             <span className="text-sm px-2">{author}</span>
           </div>
           <div className="flex flex-row items-center justify-start uppercase">
-            <FaFolder className="flex md:invisible" />
+            <FaFolder className="flex lg:invisible" />
             <Tags tags={tags} />
           </div>
           <div>
