5cd17ef90146ec9c85186011ec1abaae2478e459
Meghant00 <dangolmeghant0@gmail.com>
2021-12-02 15:27:45 +0545

    Style: Added pagination to Economy Items

--- src/components/economy/economy-items.js
+++ src/components/economy/economy-items.js
@@ -1,21 +1,50 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import news from "../../data/news";
+import { newsPerPage } from "../../utilis/global-variables";
+import Paginate from "../paginate";
 import EconomyItem from "./economy-item";
 
 const EconomyItems = () => {
+  const [recentNews, setRecentNews] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  useEffect(() => {
+    setRecentNews(news.sort((a, b) => b.date - a.date));
+  }, []);
+  const indexOfLastPost = currentPage * newsPerPage;
+  const indexOfFirstPost = indexOfLastPost - newsPerPage;
+
+  const currentNews = recentNews.slice(indexOfFirstPost, indexOfLastPost);
+
+  const paginate = (number) => {
+    setCurrentPage(number);
+  };
+  const onNextClick = () => {
+    if (currentPage < Math.ceil(recentNews.length / newsPerPage)) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
   return (
-    <div className="grid grid-cols-2 gap-8">
-      {news.map((items) => (
-        <EconomyItem
-          key={items.title}
-          title={items.title}
-          image={items.image}
-          author={items.author}
-          authorImage={items.authorImage}
-          description={items.description}
-          date={items.date}
-        />
-      ))}
+    <div>
+      <div className="grid grid-cols-2 gap-8">
+        {currentNews.map((items) => (
+          <EconomyItem
+            key={items.title}
+            title={items.title}
+            image={items.image}
+            author={items.author}
+            authorImage={items.authorImage}
+            description={items.description}
+            date={items.date}
+          />
+        ))}
+      </div>
+      <Paginate
+        posts={recentNews.length}
+        onClick={paginate}
+        postsPerPage={newsPerPage}
+        currentPage={currentPage}
+        onNextClick={onNextClick}
+      />
     </div>
   );
 };
