9609e8c71ce2afa37ac589ae18dd17b5a4f71343
Meghant00 <dangolmeghant0@gmail.com>
2021-12-15 15:29:35 +0545

    Add: Created Entertainment Component

---
+++ src/components/entertainment/entertainment.js
@@ -0,0 +1,12 @@
+import React from "react";
+import PageHeading from "../page-heading";
+
+const Entertainment = () => {
+  return (
+    <div>
+      <PageHeading heading="Entertainment" />
+    </div>
+  );
+};
+
+export default Entertainment;
